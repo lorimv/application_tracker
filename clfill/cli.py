c9033dc1180fc48ecc90e71fc5c678fcc9c10b28
lorimv <lorenzomvega@gmail.com>
2023-08-30 18:10:23 -0500

    added main call

--- clfill/cli.py
+++ clfill/cli.py
@@ -1,17 +1,7 @@
 from clfill import query
 
 def main():
-    print("Hola hola")
-
-    print("Let's start by getting the name of the company: ")
-    company_l = input()
-
-    print("yeah aight now the shortened company name: ")
-    company_s = input()
-
-    print("Position name (full)?")
-    position_l = input()
-
-    print("Now tha shortened position name")
-    position_s = input()
     query()
+
+if __name__ == '__main__':
+    main()
