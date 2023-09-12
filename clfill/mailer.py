feca2d4ef0c1137bbde2cdf534fb2ea8debd8b3a
lorimv <lorenzomvega@gmail.com>
2023-09-12 18:56:03 -0500

    mailer file added

--- clfill/mailer.py
+++ clfill/mailer.py
@@ -2,16 +2,12 @@ from . import credentials
 from googleapiclient.discovery import build
 import json
 
+# this is the module that will auto-send follow up emails when -m is called
+
 ##TODO 'documentId' environmental var?
 # TODO when should it be set up? 
 
 def method():
-    ##TODO write logic for document editor functions
+    ##TODO write logic for follow-up email functions
 
     service = build("mail", "v1", credentials=credentials)
-                                                         
-    # cl_template contains the Document we will make a copy of / TODO Template Id
-    cl_template = service.documents().get(documentId=INSERT_TEMPLATE_ID_HERE).execute()
-
-    # cl_filled is the new Document, to be filled with contents of cl_template (excluding template's Id)
-    cl_filled = service.documents().create()
