e3e552d13bc115bf1c1c173af6e08854e62b8ee6
lorimv <lorenzomvega@gmail.com>
2023-09-14 16:20:26 -0500

    added basic message

--- clfill/mailer.py
+++ clfill/mailer.py
@@ -12,3 +12,23 @@ def method():
     # TODO write logic for follow-up email functions
 
     service = build('gmail', 'v1', credentials=credentials)
+
+    message = EmailMessage()
+    message.set_content('Hello ' + COMPANY_NAME + ',\n\n'
+
+                        'My name is ' + MY_NAME + '. I am writing to follow up on my application for the ' +
+                        POSITION_NAME + ' role at ' + COMPANY_NAME + ' on [date].\n\n'
+                        'I remain excited about the possibility of joining your team and '
+                        'contributing to its success. I believe my [specific job-related skills]'
+                        ' make me a strong candidate for the position, and I would love to discuss '
+                        'the opportunity even further.\n\n'
+
+                        'Please let me know of any questions I can answer or additional information '
+                        'I can provide. Thank you for your consideration. I look forward to speaking soon!\n\n'
+
+                        'Warm regards,\n'
+                        'Lorenzo Vega')
+
+    message['To'] = COMPANY_EMAIL
+    message['From'] = MY_EMAIL
+    message['Subject'] = ('Application for ' + POSITION_NAME + ' at ' + COMPANY_NAME)
