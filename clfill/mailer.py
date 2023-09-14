78a718fcf1e42c2430d424e76ce24cb1d45cc4fb
lorimv <lorenzomvega@gmail.com>
2023-09-14 17:34:28 -0500

    tracker & mailer methods added

--- clfill/mailer.py
+++ clfill/mailer.py
@@ -1,34 +1,52 @@
-from googleapiclient.discovery import build
+import base64
 from email.message import EmailMessage
-
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 from . import credentials
 from .tracker import get_mail_info
-import json
+# import json
 
 # this is the module that will auto-send follow up emails when -m is called
-# im thinking it will have access to a tracker (spreadsheet) fn that returns company name & job title
+# im thinking it will take arguments from tracker module that returns company name & email & job title
 
-def method():
-    # TODO write logic for follow-up email functions
+def send_mail(company_name, position_name, company_email):  # TODO is it smarter to pass raw sheet data as arg?
+    # TODO write logic for follow-up email functions (different method?)
 
-    service = build('gmail', 'v1', credentials=credentials)
+    try:
+        # The following code block is the writer/sender for follow-up emails
+        service = build('gmail', 'v1', credentials=credentials)
 
-    message = EmailMessage()
-    message.set_content('Hello ' + COMPANY_NAME + ',\n\n'
+        message = EmailMessage()
+        message.set_content('Hello ' + company_name + ',\n\n'
 
-                        'My name is ' + MY_NAME + '. I am writing to follow up on my application for the ' +
-                        POSITION_NAME + ' role at ' + COMPANY_NAME + ' on [date].\n\n'
-                        'I remain excited about the possibility of joining your team and '
-                        'contributing to its success. I believe my [specific job-related skills]'
-                        ' make me a strong candidate for the position, and I would love to discuss '
-                        'the opportunity even further.\n\n'
+                            'My name is ' + MY_NAME + '. I am writing to follow up on my application for the ' +
+                            position_name + ' role at ' + company_name + ' on [date].\n\n'
+                            'I remain excited about the possibility of joining your team and '
+                            'contributing to its success. I believe my [specific job-related skills]'
+                            ' make me a strong candidate for the position, and I would love to discuss '
+                            'the opportunity even further.\n\n'
 
-                        'Please let me know of any questions I can answer or additional information '
-                        'I can provide. Thank you for your consideration. I look forward to speaking soon!\n\n'
+                            'Please let me know of any questions I can answer or additional information '
+                            'I can provide. Thank you for your consideration. I look forward to speaking soon!\n\n'
 
-                        'Warm regards,\n'
-                        'Lorenzo Vega')
+                            'Warm regards,\n'
+                            'Lorenzo Vega')  # should probably ask the user for their own name at some point
 
-    message['To'] = COMPANY_EMAIL
-    message['From'] = MY_EMAIL
-    message['Subject'] = ('Application for ' + POSITION_NAME + ' at ' + COMPANY_NAME)
+        message['To'] = company_email
+        message['From'] = MY_EMAIL
+        message['Subject'] = ('Application for ' + position_name + ' at ' + company_name)
+
+        encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
+
+        create_message = {
+            'message': {
+                'raw': encoded_message
+            }
+        }
+
+        draft = service.users().drafts().create(userId='me',
+                                                body=create_message).execute()
+    except HttpError as e:
+        print(e)
+        draft = None
+    return draft
--- clfill/tracker.py
+++ clfill/tracker.py
@@ -1,21 +1,23 @@
-from . import credentials
 from googleapiclient.discovery import build
-import json
+from . import credentials
+from .mailer import send_mail
+# import json
 
 # I think the function will CREATE a spreadsheet if not created...
 # ...then store the ID in a permanent place for future use
 
-def track():
+def update_tracker():
     # TODO write logic for document editor functions
 
     service = build('sheets', 'v4', credentials=credentials)
     if ():  # the place where we store tracker's docId is empty...
         tracker = service.sheets().create()
         # store tracker's id
+    
 
-def get_mail_info():
+def get_email_info():
     # returns the email, company name, job position from any non-followed up applications    
-    if ():  # the place where we store tracker's docId is empty...
+    if ():  # TODO the place where we store tracker's docId is empty...
         return []
 
     # up here we will configure 'range' vars to include columns for company, position, and emails
@@ -23,11 +25,17 @@ def get_mail_info():
     # check if an app has been followed up ('No') ->
     # check if an app is ready to be followed up (app date >= today's date) ->
     # output job title, company name, email address
-    mailinfo = []
-    service = build('sheets', 'v4', credentials=credentials)
-    result = service.spreadsheets().values().batchGet(
-        spreadsheetId=OURSHEETID, ranges=INSERTRANGEHERE).execute()
+    email_info = []
+    
+    for (n) in (e):  # TODO edit logic here to add each valid row to email_info array
+        service = build('sheets', 'v4', credentials=credentials)
+        result = service.spreadsheets().values().batchGet(
+            spreadsheetId=OURSHEETID, ranges=INSERTRANGEHERE).execute()
+        email_info.append(result)
     return result
-    # here's where we add company name, job position, email from any job w/ "No" in followed up section
 
-    return mailinfo
+def email_scheduler(email_info):
+    # this fn will be responsible for calling send_mail on each necessary row
+    # for each spreadsheet row in email_info list...
+    send_mail()
+    return
