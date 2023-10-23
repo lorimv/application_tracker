b21f5623a6cb99aba3e82ac4ae0de671f00018a2
lorimv <lorenzomvega@gmail.com>
2023-10-23 16:29:33 -0500

    anooother syncccc

--- clfill/cli.py
+++ clfill/cli.py
@@ -21,15 +21,9 @@ from .mailer import send_mail
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description='cover letter filler')
-    parser.add_argument('bait', help='Stand-in arg.')
-    parser.add_argument('switch', help='Stand-in arg.')
-    parser.add_argument('output', help='Stand-in arg.')
     parser.add_argument('-v', '--verbose', help='Verbose output.', action='store_true')
     parser.add_argument('-i', '--ignore-path-check', help="Stand-in arg.\n" +
                         "Stand-in arg -- line 2", action="store_true")
-    parser.add_argument('-dt', '--dont-use-tempdir', help="Prevent the tool from creating a temporary directory when creating the exploit.\n" +
-                        "Instead, create the intermediate folders in current working directory.", action="store_true")
-    parser.add_argument("-pt", "--preserve-temp", help="Preserve the temporary directory after creating the exploit. Has no effect if -dt or --dont-use-tempdir is not specified.")
     return parser.parse_args()
 
 def query():
@@ -56,6 +50,7 @@ def test():
 
 
 def main():
+    options = parse_args()
     # good enough for now
     test()
     # method()  # maybe arguments into filler (docs)  & tracker (sheets), mailer will use info from spreadsheet when -m
